4c66fa863857345117c7e715fa56b782ffda2836
linkcoding <kenny.lin.91@gmail.com>
2017-09-08 10:12:05 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
-//define functions here
+function getIt() {
+  $('p').on('click', ()=>{
+    alert("Hey!")
+  })
+}
+
+function frameIt(){
+  $('img').on('load', ()=>{
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt(){
+  $('#typing').on('keydown', (event)=>{
+    if(event.which === 71){
+      alert("You pressed g!")
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit', (e)=> {
+    e.preventDefault()
+    alert("Your form is going to be submitted now.")
+  })
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+  getIt()
+  frameIt()
+  pressIt()
+  submitIt()
 });
